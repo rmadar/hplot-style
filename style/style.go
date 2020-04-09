f23de10082b19f5daa8a939bd55e9c2dcfb844f1
rmadar <romain.madar@gmail.com>
2020-04-09 11:25:40 +0200

    Minor update style

--- style/style.go
+++ style/style.go
@@ -9,7 +9,9 @@ import (
 	"go-hep.org/x/hep/hplot"
 )
 
-var defaultBlack = color.NRGBA{R: 35, G: 55, B: 57, A: 255}
+var (
+	SmoothBlack = color.NRGBA{R: 35, G: 55, B: 57, A: 255}
+)
 
 // Apply nice style for the plot
 func ApplyToPlot(p *hplot.Plot){
@@ -22,7 +24,7 @@ func ApplyToPlot(p *hplot.Plot){
 
 	// Specify title style
 	p.Title.TextStyle.Font.Size = 16
-	p.Title.TextStyle.Color = defaultBlack
+	p.Title.TextStyle.Color = SmoothBlack
 	p.Title.Padding = 10
 
 	// Specify axis ranges and padding (ie distance to (0,0) point
@@ -32,33 +34,50 @@ func ApplyToPlot(p *hplot.Plot){
 	// Specify axis label & fontsize
 	p.X.Label.TextStyle.Font.Size = 14
 	p.Y.Label.TextStyle.Font.Size = 14
-	p.X.Label.TextStyle.Color = defaultBlack
-	p.Y.Label.TextStyle.Color = defaultBlack
+	p.X.Label.TextStyle.Color = SmoothBlack
+	p.Y.Label.TextStyle.Color = SmoothBlack
 
 	// Specify axis style
 	p.X.LineStyle.Width = 1.05
 	p.Y.LineStyle.Width = 1.05
-	p.X.LineStyle.Color = defaultBlack
-	p.Y.LineStyle.Color = defaultBlack
+	p.X.LineStyle.Color = SmoothBlack
+	p.Y.LineStyle.Color = SmoothBlack
 
 	// Specify ticks style
 	p.X.Tick.LineStyle.Width = 1.05
 	p.Y.Tick.LineStyle.Width = 1.05
-	p.X.Tick.LineStyle.Color = defaultBlack
-	p.Y.Tick.LineStyle.Color = defaultBlack
+	p.X.Tick.LineStyle.Color = SmoothBlack
+	p.Y.Tick.LineStyle.Color = SmoothBlack
 	p.X.Tick.Label.Font.Size = 11
 	p.Y.Tick.Label.Font.Size = 11
-	p.X.Tick.Label.Color = defaultBlack
-	p.Y.Tick.Label.Color = defaultBlack
+	p.X.Tick.Label.Color = SmoothBlack
+	p.Y.Tick.Label.Color = SmoothBlack
 
 	// Specify tick position
 	p.X.Tick.Marker = hplot.Ticks{N: 10}
 	p.Y.Tick.Marker = hplot.Ticks{N: 10}
-
+	
 	// Specify text style of the legend
 	p.Legend.TextStyle.Font.Size = 14
-	p.Legend.TextStyle.Color = defaultBlack
+	p.Legend.TextStyle.Color = SmoothBlack
 	p.Legend.Padding = 0.1 * vg.Inch
+}
+
+// Apply cirle markers
+func SetCircleMarkersTo(h *hplot.H1D){
+
+	// Dots as marker
+	h.GlyphStyle = draw.GlyphStyle{
+		Shape: draw.CircleGlyph{},
+		Color: SmoothBlack,
+		Radius: vg.Points(3)}
+
+	// Y error bars
+	if h.YErrs != nil {
+		h.YErrs.LineStyle.Color = SmoothBlack
+		h.YErrs.LineStyle.Width = 2.5
+		h.YErrs.CapWidth = 7
+	}
 }
 
 // Apply nominal style for histogram representing data
@@ -69,17 +88,8 @@ func ApplyToDataHist(hData *hplot.H1D){
 	
 	// No line
 	hData.LineStyle.Width = 0
-
-	// Y error bars
-	if hData.YErrs != nil {
-		hData.YErrs.LineStyle.Color = color.NRGBA{R: 10, G: 10, B: 10, A: 255}
-		hData.YErrs.LineStyle.Width = 2.5
-		hData.YErrs.CapWidth = 7
-	}
 	
-	// Dots as marker
-	hData.GlyphStyle = draw.GlyphStyle{
-		Shape:  draw.CircleGlyph{},
-		Color: color.NRGBA{R: 10, G: 10, B: 10, A: 255},
-		Radius: vg.Points(3)}
+	// Apply circles as marker
+	SetCircleMarkersTo(hData)
+
 }
