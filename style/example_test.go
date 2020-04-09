b18b6a15cdb24f30e2b1a2c2bd1b1a6a0d3a9584
rmadar <romain.madar@gmail.com>
2020-04-09 11:26:04 +0200

    Adding an example

---
+++ style/example_test.go
@@ -0,0 +1,54 @@
+package style_test
+
+import (
+	"log"
+	"fmt"
+	
+	"gonum.org/v1/gonum/stat/distuv"
+	"golang.org/x/exp/rand"
+
+	"gonum.org/v1/plot/vg"
+	
+	"go-hep.org/x/hep/hbook"
+	"go-hep.org/x/hep/hplot"
+
+	"github.com/rmadar/hplot-style/style"
+)
+
+func ExampleSimePlot() {
+	
+	// Create a very wide normal distribution
+	dist := distuv.Normal{
+		Mu:    0,
+		Sigma: 5,
+		Src:   rand.New(rand.NewSource(0)),
+	}
+
+	// Histogram filling
+	h:= hbook.NewH1D(20, -5, 5)
+	for i := 0; i < 50000; i++ {
+		v := dist.Rand()
+		h.Fill(v, 1)
+	}
+
+	// Histogram scaling
+	h.Scale(1.0/h.Integral())
+
+	// Making the plot
+	p := hplot.New()
+	style.ApplyToPlot(p)
+	hist := hplot.NewH1D(h)
+	p.Add(hist)
+	p.Y.Min = 0
+
+	// Saving the result
+	if err := p.Save(6*vg.Inch, -1, "example_plot.png"); err != nil {
+		log.Fatalf("error saving plot: %v\n", err)
+	} else {
+		fmt.Println("Plot saved.")
+	}
+
+	// Output:
+	// Plot saved.
+}
+
